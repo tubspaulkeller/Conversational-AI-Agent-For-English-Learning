6b26dc188646ad49fe68a88c4da934895c34bc36
PaulKeller94 <102319452+PaulKeller94@users.noreply.github.com>
2022-12-16 17:29:47 +0100

    added not repeating 'hallo'

--- actions/helper/get_user_credentials.py
+++ actions/helper/get_user_credentials.py
@@ -1,8 +1,7 @@
 from actions.common.slack import get_user
 from typing import (Text)
 from rasa_sdk import Action
-from rasa_sdk.events import SlotSet, FollowupAction
-
+from rasa_sdk.events import SlotSet, FollowupAction, UserUtteranceReverted
 ##########################################################################################
 # Get user credentials
 ##########################################################################################
@@ -14,12 +13,16 @@ class ActionGetUserCredentials(Action):
 
     async def run(self, dispatcher, tracker, domain):
         """ get user credentials """
-        first_name = await get_user(tracker.sender_id, tracker)
-        if first_name != None:
-            dispatcher.utter_message(
-                f"Hey {first_name}! 😊\nMein Name ist Ben und ich bin dein persönlicher Assistent. Ich helfe dir dabei, deine Englisch-Fähigkeiten zu verbessern. 🤖\nFrage mich gerne jeder Zeit zu deinen:\n- erzielten Punkten 🎯\n- gesammelten Sterne 🌟\n- verdienten Abzeichen 🎖\n\n Mit 'Was war die letzte Frage' o.ä. kehren wir anschließend zur Quiz-Frage zurück.\nUm mich neuzustarten, tippe bitte 'restart' o.ä. ein. 😎")
-            return [SlotSet("first_name", first_name), FollowupAction("action_set_reminder_set_dp")]
+        if tracker.get_slot("first_name") is None:
+            first_name = await get_user(tracker.sender_id, tracker)
+            if first_name != None:
+                dispatcher.utter_message(
+                    f"Hey {first_name}! 😊\nMein Name ist Ben und ich bin dein persönlicher Assistent. Ich helfe dir dabei, deine Englisch-Fähigkeiten zu verbessern. 🤖\nFrage mich gerne jeder Zeit zu deinen:\n- erzielten Punkten 🎯\n- gesammelten Sterne 🌟\n- verdienten Abzeichen 🎖\n\n Mit 'Was war die letzte Frage' o.ä. kehren wir anschließend zur Quiz-Frage zurück.\nUm mich neuzustarten, tippe bitte 'restart' o.ä. ein. 😎")
+                return [SlotSet("first_name", first_name), FollowupAction("action_set_reminder_set_dp")]
+            else:
+                dispatcher.utter_message(
+                    f"Hey Buddy! 😊\nMein Name ist Ben und ich bin dein persönlicher Assistent. Ich helfe dir dabei, deine Englisch-Fähigkeiten zu verbessern. 🤖\nFrage mich gerne jeder Zeit zu deinen:\n- erzielten Punkte 🎯\n- gesammelten Sterne 🌟\n- verdienten Abzeichen 🎖\n\n Mit 'Was war die letzte Frage' o.ä. kehren wir anschließend zur Quiz-Frage zurück.\nUm mich neuzustarten, tippe bitte 'restart' o.ä. ein. 😎")
+                return [SlotSet("first_name", "Buddy"), FollowupAction("action_set_reminder_set_dp")]
         else:
-            dispatcher.utter_message(
-                f"Hey Buddy! 😊\nMein Name ist Ben und ich bin dein persönlicher Assistent. Ich helfe dir dabei, deine Englisch-Fähigkeiten zu verbessern. 🤖\nFrage mich gerne jeder Zeit zu deinen:\n- erzielten Punkte 🎯\n- gesammelten Sterne 🌟\n- verdienten Abzeichen 🎖\n\n Mit 'Was war die letzte Frage' o.ä. kehren wir anschließend zur Quiz-Frage zurück.\nUm mich neuzustarten, tippe bitte 'restart' o.ä. ein. 😎")
-            return [SlotSet("first_name", "Buddy"), FollowupAction("action_set_reminder_set_dp")]
+            print("DEBUG: USER CREDENTIALS ALREADY SET")
+            return [UserUtteranceReverted()]
