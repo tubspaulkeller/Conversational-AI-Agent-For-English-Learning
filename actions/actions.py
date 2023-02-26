1c6a0daf5cbb4deeed147ecabe8fa48f72f6000a
PaulKeller94 <102319452+PaulKeller94@users.noreply.github.com>
2023-02-26 22:53:56 +0100

    added button for starting

--- actions/actions.py
+++
@@ -1,5 +0,0 @@
-from typing import Any, Text, Dict, List
-from rasa_sdk import Action, Tracker, FormValidationAction
-from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import UserUtteranceReverted, FollowupAction, AllSlotsReset, Restarted
-
--- actions/helper/get_user_credentials.py
+++ actions/helper/get_user_credentials.py
@@ -16,18 +16,29 @@ class ActionGetUserCredentials(Action):
 
     async def run(self, dispatcher, tracker, domain):
         """ get user credentials """
+        lets_go_button = [
+            {'title': "Let's go! 🚀", 'payload': '/i_lets_go{\"e_lets_go\":\"LETS_GO\"}'}]
         if tracker.get_slot("first_name") is None:
             first_name = await get_user(tracker.sender_id, tracker)
             if first_name != None:
-                text = "Hi %s! 😊,\n ich bin dein Buddy Ben und ich werde dich während des Englischtrainings mit *Punkten*, *Sternen* und *Abzeichen* belohnen, damit du immer motiviert bleibst und deine Fortschritte feiern kannst. 🎉\n\n Jedes Mal, wenn du eine Frage richtig beantwortest oder eine Lektion abschließt, wirst du Punkte sammeln und Sterne verdienen, um zu zeigen, wie gut du dich schlägst. Wenn du bestimmte Ziele erreichst, wirst du außerdem Abzeichen freischalten, die deine Leistungen noch weiter unterstreichen.\n\nEgal ob du gerade erst anfängst oder schon ein fortgeschrittener Englischlerner bist - meine Belohnungen werden dir helfen, motiviert und engagiert zu bleiben. Und wenn du mal eine Frage verpasst oder Hilfe brauchst, stehe ich dir immer zur Seite.\nWenn du mehr über meine Fähigkeiten erfahren möchtest, dann schau doch mal unter *'Skills'* nach! Oder frage mich jederzeit nach diesen.\n\nLass uns gemeinsam dein Englisch verbessern und dabei auch noch jede Menge Spaß haben! Let's go! 🚀" % first_name
+                text = "Hi %s! 😊,\n ich bin dein Buddy Ben und ich werde dich während des Englischtrainings mit *Punkten*, *Sternen* und *Abzeichen* belohnen, damit du immer motiviert bleibst und deine Fortschritte feiern kannst. 🎉\n\n Jedes Mal, wenn du eine Frage richtig beantwortest oder eine Lektion abschließt, wirst du Punkte sammeln und Sterne verdienen, um zu zeigen, wie gut du dich schlägst. Wenn du bestimmte Ziele erreichst, wirst du außerdem Abzeichen freischalten, die deine Leistungen noch weiter unterstreichen.\n\nEgal ob du gerade erst anfängst oder schon ein fortgeschrittener Englischlerner bist - meine Belohnungen werden dir helfen, motiviert und engagiert zu bleiben. Und wenn du mal eine Frage verpasst oder Hilfe brauchst, stehe ich dir immer zur Seite.\nWenn du mehr über meine Fähigkeiten erfahren möchtest, dann schau doch mal unter *'Skills'* nach! Oder frage mich jederzeit nach diesen.\n\nLass uns gemeinsam dein Englisch verbessern und dabei auch noch jede Menge Spaß haben!" % first_name
                 dispatcher.utter_message(
                     json_message=markdown_formatting(text))
-                return [SlotSet("first_name", first_name), FollowupAction("action_set_reminder_set_dp")]
+
+                dispatcher.utter_message(
+                    text=" ", buttons=lets_go_button)
+
+                # FollowupAction("action_set_reminder_set_dp")]
+                # SlotSet("s_get_dp_form", None), SlotSet("s_set_next_form", None), FollowupAction("get_dp_form")]
+                return [SlotSet("first_name", first_name)]
             else:
                 text = "Hi,\n ich bin dein Buddy Ben und ich werde dich während des Englischtrainings mit *Punkten*, *Sternen* und *Abzeichen* belohnen, damit du immer motiviert bleibst und deine Fortschritte feiern kannst. 🎉\n\n Jedes Mal, wenn du eine Frage richtig beantwortest oder eine Lektion abschließt, wirst du Punkte sammeln und Sterne verdienen, um zu zeigen, wie gut du dich schlägst. Wenn du bestimmte Ziele erreichst, wirst du außerdem Abzeichen freischalten, die deine Leistungen noch weiter unterstreichen.\n\nEgal ob du gerade erst anfängst oder schon ein fortgeschrittener Englischlerner bist - meine Belohnungen werden dir helfen, motiviert und engagiert zu bleiben. Und wenn du mal eine Frage verpasst oder Hilfe brauchst, stehe ich dir immer zur Seite.\nWenn du mehr über meine Fähigkeiten erfahren möchtest, dann schau doch mal unter *'Skills'* nach Oder frage mich jederzeit nach diesen!\n\nLass uns gemeinsam dein Englisch verbessern und dabei auch noch jede Menge Spaß haben! Let's go! 🚀"
                 dispatcher.utter_message(
                     json_message=markdown_formatting(text))
-                return [SlotSet("first_name", "Buddy"), FollowupAction("action_set_reminder_set_dp")]
+                dispatcher.utter_message(
+                    text=" ", buttons=lets_go_button)
+                return [SlotSet("first_name", first_name)]
+                # return [SlotSet("first_name", "Buddy"), FollowupAction("action_set_reminder_set_dp")]
         else:
             print("DEBUG: USER CREDENTIALS ALREADY SET",
                   tracker.latest_message['text'].lower())
