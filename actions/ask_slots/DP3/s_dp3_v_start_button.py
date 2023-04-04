0cf219dd3d75e90a356b076660b1f53bdf2cedcc
PaulKeller94 <102319452+PaulKeller94@users.noreply.github.com>
2023-04-04 23:03:29 +0200

    typo

--- actions/ask_slots/DP3/s_dp3_v_start_button.py
+++ actions/ask_slots/DP3/s_dp3_v_start_button.py
@@ -13,7 +13,7 @@ class AskForSlotAction(Action):
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
             domain: Dict) -> List[EventType]:
         # user can change his goal
-        text = "Lass uns nun mit dem Quiz beginnen!\n*Spielregel*: Für eine korrekte Antwort erhälst du 5 Punkte und für eine korrekte Antwort im zweiten Versuch erhälst du 2 Punkte. Du bekommst zwei Chancen von mir die Frage richtig zu beantworten."
+        text = "Lass uns nun mit dem Quiz beginnen!\n*Spielregel*: Für eine korrekte Antwort erhälst du 5 Punkte und für eine korrekte Antwort im zweiten Versuch erhälst du 2 Punkte. Du bekommst zwei Chancen von mir, die Frage richtig zu beantworten."
         dispatcher.utter_message(json_message=markdown_formatting(text))
         dispatcher.utter_message(response="utter_s_dp3_v_start_button")
         return []
