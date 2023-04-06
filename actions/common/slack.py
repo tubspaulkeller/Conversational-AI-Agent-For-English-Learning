7f587208e53676dba76d8a4074b2a150730616c0
PaulKeller94 <102319452+PaulKeller94@users.noreply.github.com>
2023-04-06 19:41:52 +0200

    added_token

--- actions/common/slack.py
+++ actions/common/slack.py
@@ -5,14 +5,16 @@ import os
 import json
 load_dotenv()
 
-client = AsyncWebClient(
-    token=os.getenv('SLACK_TOKEN'))
+# client = AsyncWebClient(
+#     token=_get_token()))
+# here muss SLACK_TOKEN_TWO, SLACK_TOKEN_THREE,..
 
 
 async def get_user(id, tracker):
     print("API CALL: GET USER")
     try:
         if tracker.get_slot("first_name") is None:
+            client = AsyncWebClient(token=_get_token())
             user_cred = await client.users_info(user=id)
             return user_cred['user']['profile']['first_name']
             # l_name = user_cred['user']['profile']['last_name']
@@ -23,3 +25,11 @@ async def get_user(id, tracker):
     except Exception as e:
         print("ERROR: GETING USER ", e)
         return None
+
+
+def _get_token():
+    try:
+        return os.environ['SLACK_TOKEN'] if os.environ['SLACK_TOKEN'] is not None else os.getenv('SLACK_TEST_TOKEN')
+
+    except:
+        return os.getenv('SLACK_TEST_TOKEN')
